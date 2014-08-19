f0e8cd76973a434ff0b4473c3a0a90689d668f7a
jongrover <hello@jonathangrover.com>
2014-08-18 20:08:15 -0400

    remove solution

--- js/calc.js
+++ js/calc.js
@@ -1,37 +1,5 @@
 $(function(){
 
-  var number1       = NaN,
-      number2       = NaN,
-      operation     = null,
-      $number1      = $('#number1'),
-      $number2      = $('#number2'),
-      $operation    = $('#operation'),
-      $equalsButton = $('#equals'),
-      $result       = $('#result');
-
-  function result() {
-    switch(operation) {
-      case '+': $result.text(number1 + number2); break;
-      case '-': $result.text(number1 - number2); break;
-      case '*': $result.text(number1 * number2); break;
-      case '/': $result.text(number1 / number2); break;
-      default : $result.text('Sorry, not a valid operation!');
-    }
-  }
-
-  function validate() {
-    if (isNaN(number1) || number1 === '' || isNaN(number2) || number2 === '' ) {
-      $result.text('Sorry, one of those is not a valid number!');
-    } else {
-      result();
-    }
-  }
-
-  $equalsButton.click(function() {
-    number1 = parseFloat($number1.val());
-    number2 = parseFloat($number2.val());
-    operation = $operation.val();
-    validate();
-  });
+  // write your solution here.
 
 });
