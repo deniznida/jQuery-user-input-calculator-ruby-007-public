e575b2ac28e74f9af88c5c719c2ab067fffaeae8
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-11-06 12:30:44 -0500

    finishes tests

--- README.md
+++ README.md
@@ -20,10 +20,21 @@ Students will work in groups to create a calculator that accepts user input usin
 
   The calculator should do the following:  
     1. Be object oriented.
-    1. the calculator should take input for 2 separate numbers and 1 operation: '+', '-', '*', '/', and store them as variables.
+    1. the calculator should take input for 2 separate numbers and 1 operation: '+', '-', '*', '/'.
     2. When the user clicks the equals button, the calculator should update a result variable based on the operation choosen and the value of the two numbers. It should then report the result into the h2 with an id="result". If the user leaves a number field blank or inserts some input that is not a number then h2 with id="result" should instead print the error message: "Sorry, one of those is not a valid number!", or if the operation inserted is not one of the valid accepted operations then the error message should then read "Sorry, not a valid operation!"
 
 See a live working example of the simple calculator [here](http://flatiron-school-curriculum.github.io/fe-jquery-user-input-calc/)
+
+##Before You Start
+```shell
+# Install jasmine:
+gem sources -a http://flatiron:33west26@gems.flatironschool.com
+gem install ironboard
+
+# Run Jasmine test suite:
+ironboard  # runs only in the terminal
+ironboard -b # runs also in the browser
+```
 
 ## Resources
 
--- spec/calc-spec.js
+++ spec/calc-spec.js
@@ -6,4 +6,68 @@ describe('Calculator', function() {
     var calculator = new Calculator();
   });
 
+  describe('validating inputs', function() {
+    describe('validates numbers', function() {
+      it('should validate the first numerical input and display an error if they are not a number', function() {
+        $('#number1').val('taco');
+        $('#operation').val('+');
+        $('#number2').val(4);
+        $('#equals').click();
+        expect($('#result').text()).toBe('Sorry, one of those is not a valid number!');
+      });
+      it('should validate the second numerical input and display an error if they are not a number', function() {
+        $('#number1').val(3);
+        $('#operation').val('+');
+        $('#number2').val('flibber flabber');
+        $('#equals').click();
+        expect($('#result').text()).toBe('Sorry, one of those is not a valid number!');
+      });
+    });
+    describe('validates operators', function() {
+      it('should validate that a +, -, /, or * was used', function() {
+        $('#number1').val(3);
+        $('#operation').val('marshmallow');
+        $('#number2').val(5);
+        $('#equals').click();
+        expect($('#result').text()).toBe('Sorry, not a valid operation!');
+      });
+      it('should validate that only one +, -, /, or * was used', function() {
+        $('#number1').val(3);
+        $('#operation').val('++');
+        $('#number2').val(5);
+        $('#equals').click();
+        expect($('#result').text()).toBe('Sorry, not a valid operation!');
+      });
+    });
+  });
+  describe('Does math', function() {
+    it('should be able to add', function() {
+      $('#number1').val(3);
+      $('#operation').val('+');
+      $('#number2').val(5);
+      $('#equals').click();
+      expect($('#result').text()).toBe('8');
+    });
+    it('should be able to subtract', function() {
+      $('#number1').val(3);
+      $('#operation').val('-');
+      $('#number2').val(5);
+      $('#equals').click();
+      expect($('#result').text()).toBe('-2');
+    });
+    it('should be able to divide', function() {
+      $('#number1').val(15);
+      $('#operation').val('/');
+      $('#number2').val(5);
+      $('#equals').click();
+      expect($('#result').text()).toBe('3');
+    });
+    it('should be able to multiply', function() {
+      $('#number1').val(3);
+      $('#operation').val('*');
+      $('#number2').val(5);
+      $('#equals').click();
+      expect($('#result').text()).toBe('15');
+    });
+  });
 });
